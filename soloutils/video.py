38e7f6830f6d171dcb6b4a2539b1cab732135024
Tim Ryan <tim@3drobotics.com>
2015-12-13 22:20:33 -0800

    Updates video script to require reboot.

--- soloutils/video.py
+++ soloutils/video.py
@@ -2,32 +2,31 @@ import paramiko, base64, time, sys, soloutils
 from distutils.version import LooseVersion
 
 ACQUIRE = """
-TOGGLE="if [ -e /home/root/ACQUIRE_VIDEO_FEED ]; then while true; do sleep 100000; done; fi"
-
 echo 'acquiring video feed...'
-grep -q "while true; do sleep 100000; done" /usr/bin/video_send.sh || {
-    cp /usr/bin/video_send.sh /usr/bin/video_send.sh.bkp
-    echo "#!/bin/sh" > /usr/bin/video_send.sh
-    echo "$TOGGLE" >> /usr/bin/video_send.sh
-    echo "" >> /usr/bin/video_send.sh
-    cat /usr/bin/video_send.sh.bkp >> /usr/bin/video_send.sh
+if [ ! -e /usr/bin/video_send_logic.sh ]; then
+    cp /usr/bin/video_send.sh /usr/bin/video_send_logic.sh
+    echo '#!/bin/bash
+if [ -e /home/root/ACQUIRE_VIDEO_FEED ]; then
+    while true; do sleep 100000; done;
+else
+    exec /usr/bin/video_send_logic.sh
+done' > /usr/bin/video_send.sh
 }
 
 touch ~/ACQUIRE_VIDEO_FEED
-killall video_send.sh sn_master_snd sn_sender sn_pktsnd gst-launch-0.10 2>/dev/null
-init q
 echo '/dev/video0 will be available for use by user scripts after reboot.'
 echo 'rebooting...'
 sync
-reboot
+sololink_config --reboot
 """
 
 RESTORE = """
 echo 'restoring video feed...'
 rm ~/ACQUIRE_VIDEO_FEED 2> /dev/null
-killall video_send.sh sn_master_snd sn_sender sn_pktsnd gst-launch-0.10 2>/dev/null
-init q
-echo '/dev/video0 reclaimed for the video downlink.'
+echo 'video feed will be restored after reboot.'
+echo 'rebooting...'
+sync
+sololink_config --reboot
 """
 
 def main(args):
